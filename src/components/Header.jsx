6cb9ad2f7ea5b85156bce3e448b3a9b4fa536af2
Abdullahi Ismail <abdullahabuaslam@gmail.com>
2023-07-15 03:26:04 +0100

    css adjustments

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,14 @@ import{MdMonochromePhotos} from "react-icons/md";
 
 const Header = ({ onSearch }) => {
   return (
-    <>
+    <div className="head">
       <div className="logo">
         <MdMonochromePhotos className="ic"/>
         <h1>ImGallery</h1>
       </div>
       <SearchImage onSearch={onSearch} />
       <Categories />
-    </>
+    </div>
   );
 };
 
