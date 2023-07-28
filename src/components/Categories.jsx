6e098ed997f4f79b91c774bb26dcf466368827d7
Abdullahi Ismail <abdullahabuaslam@gmail.com>
2023-07-28 05:57:59 +0100

    categories updated

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,17 +1,50 @@
-
+import { useState } from "react";
 
 const Categories = ({ onSearch }) => {
+  const [activeCategory, setActiveCategory] = useState("Random");
   const handlebtnClick = (e) => {
-    onSearch(e.target.value)
+    const selectedCategory = e.target.value;
+    onSearch(e.target.value);
+    setActiveCategory(selectedCategory)
   };
   return (
     <section className="categories">
-      <input type="submit" className="list active" value={"Random"} onClick={handlebtnClick}/>
-      <input type="submit" className="list" value={"Animals"} onClick={handlebtnClick}/>
-      <input type="submit" className="list" value={"Sports"} onClick={handlebtnClick}/>
-      <input type="submit" className="list" value={"Nature"} onClick={handlebtnClick}/>
-      <input type="submit" className="list" value={"Culture"} onClick={handlebtnClick}/>
-      <input type="submit" className="list" value={"School"} onClick={handlebtnClick}/>
+      <input
+        type="submit"
+        className={activeCategory === "Random" ? "list active" : "list"}
+        value={"Random"}
+        onClick={handlebtnClick}
+      />
+      <input
+        type="submit"
+        className={activeCategory === "Random" ? "list active" : "list"}
+        value={"Animals"}
+        onClick={handlebtnClick}
+      />
+      <input
+        type="submit"
+        className={activeCategory === "Random" ? "list active" : "list"}
+        value={"Sports"}
+        onClick={handlebtnClick}
+      />
+      <input
+        type="submit"
+        className={activeCategory === "Random" ? "list active" : "list"}
+        value={"Nature"}
+        onClick={handlebtnClick}
+      />
+      <input
+        type="submit"
+        className={activeCategory === "Random" ? "list active" : "list"}
+        value={"Culture"}
+        onClick={handlebtnClick}
+      />
+      <input
+        type="submit"
+        className={activeCategory === "Random" ? "list active" : "list"}
+        value={"School"}
+        onClick={handlebtnClick}
+      />
     </section>
   );
 };
