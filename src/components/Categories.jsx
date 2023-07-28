6f58c3565bea542f53aea16f4d72444884b80d52
Abdullahi Ismail <abdullahabuaslam@gmail.com>
2023-07-28 06:06:22 +0100

    categories updated

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -5,7 +5,8 @@ const Categories = ({ onSearch }) => {
   const handlebtnClick = (e) => {
     const selectedCategory = e.target.value;
     onSearch(e.target.value);
-    setActiveCategory(selectedCategory)
+    setActiveCategory(selectedCategory);
+    console.log(activeCategory)
   };
   return (
     <section className="categories">
@@ -17,31 +18,31 @@ const Categories = ({ onSearch }) => {
       />
       <input
         type="submit"
-        className={activeCategory === "Random" ? "list active" : "list"}
+        className={activeCategory === "Animals" ? "list active" : "list"}
         value={"Animals"}
         onClick={handlebtnClick}
       />
       <input
         type="submit"
-        className={activeCategory === "Random" ? "list active" : "list"}
+        className={activeCategory === "Sports" ? "list active" : "list"}
         value={"Sports"}
         onClick={handlebtnClick}
       />
       <input
         type="submit"
-        className={activeCategory === "Random" ? "list active" : "list"}
+        className={activeCategory === "Nature" ? "list active" : "list"}
         value={"Nature"}
         onClick={handlebtnClick}
       />
       <input
         type="submit"
-        className={activeCategory === "Random" ? "list active" : "list"}
+        className={activeCategory === "Culture" ? "list active" : "list"}
         value={"Culture"}
         onClick={handlebtnClick}
       />
       <input
         type="submit"
-        className={activeCategory === "Random" ? "list active" : "list"}
+        className={activeCategory === "School" ? "list active" : "list"}
         value={"School"}
         onClick={handlebtnClick}
       />
